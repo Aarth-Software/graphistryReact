6cd8fd73f28deaa318ec98f2aeaaf8dd72f0e5bc
Srinivas Chary <srinivasa@aarth.io>
2022-12-27 15:30:57 +0530

    fix neo4j connetion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Graphistry } from "@graphistry/client-api-react";
+import "@graphistry/client-api-react/assets/index.css";
+import "./App.css";
+import NeoFun from "./NeoFun";
+import neo4j from "neo4j-driver";
 function App() {
+  const [data, setData] = React.useState([]);
+  const [status, setStatus] = React.useState(false);
+  const getData = async () => {
+    setStatus(false);
+    const readQuery = `MATCH (n:Movie) RETURN n LIMIT 25`;
+    const config = {
+      neo4j: {
+        url: "neo4j+s://7a396979.databases.neo4j.io",
+        authUser: "neo4j",
+        authKey: "MyHTf2fSrN-KgLEPwJmuCw6Z1dcqGOITyyd5wCHpGZ0",
+      },
+    };
+    const driver = neo4j.driver(
+      config.neo4j.url,
+      neo4j.auth.basic(config.neo4j.authUser, config.neo4j.authKey)
+    );
+    const session = driver.session({ database: "neo4j" });
+    var result = null;
+    try {
+      result = await session.run(readQuery);
+      console.log(result);
+      setStatus(true);
+    } catch (error) {
+      console.log(`unable to execute query. ${error}`);
+      setStatus(false);
+    } finally {
+      console.log("connected");
+      await session.close();
+    }
+    setData(result.records);
+  };
+  React.useEffect(() => {
+    getData();
+    console.log(data);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="InputContainer">
+        <textarea />
+      </div>
+      <div className="graphistryContainer">
+        <Graphistry
+          dataset="Miserables"
+          graphistryHost="https://hub.graphistry.com"
+        />
+      </div>
     </div>
   );
 }
---
+++ src/NeoFun.js
@@ -0,0 +1,56 @@
+import React from "react";
+import neo4j from "neo4j-driver";
+
+const NeoFun = () => {
+  const config = {
+    neo4j: {
+      url: "neo4j+s://7a396979.databases.neo4j.io",
+      authUser: "neo4j",
+      authKey: "MyHTf2fSrN-KgLEPwJmuCw6Z1dcqGOITyyd5wCHpGZ0",
+    },
+  };
+  const driver = neo4j.driver(
+    config.neo4j.url,
+    neo4j.auth.basic(config.neo4j.authUser, config.neo4j.authKey)
+  );
+
+  const getData = async () => {
+    try {
+      const movieTitle = "Toy Story";
+      await findPerson(driver, movieTitle);
+      // await findPerson(driver, person2Name);
+    } catch (error) {
+      console.error(`Something went wrong: ${error}`);
+    } finally {
+      // Don't forget to close the driver connection when you're finished with it.
+      await driver.close();
+    }
+    async function findPerson(driver, ele) {
+      const session = driver.session({ database: "neo4j" });
+
+      try {
+        const readQuery = `
+            MATCH (p:Person)-[:DIRECTED]->(:Movie {title: $ele})
+            RETURN p.name As Director
+          `;
+
+        const readResult = await session.executeRead((tx) =>
+          tx.run(readQuery, { ele })
+        );
+        console.log(readResult.records);
+
+        readResult.records.forEach((record) => {
+          console.log(`${record.get("p")}`);
+        });
+      } catch (error) {
+        console.error(`Something went wrong: ${error}`);
+      } finally {
+        await session.close();
+      }
+    }
+  };
+  getData();
+  return <div>hello</div>;
+};
+
+export default NeoFun;
